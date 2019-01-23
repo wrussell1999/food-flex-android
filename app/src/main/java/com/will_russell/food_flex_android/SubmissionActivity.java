0543103e4ed43778526a32db1feb236b5b0831e1
wrussell1999 <jetpowerss@gmail.com>
2019-01-23 02:47:45 +0000

    Upload button opens device internal gallery

--- app/src/main/java/com/will_russell/food_flex_android/SubmissionActivity.java
+++ app/src/main/java/com/will_russell/food_flex_android/SubmissionActivity.java
@@ -1,18 +1,58 @@
 package com.will_russell.food_flex_android;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import android.view.View;
 import android.os.Bundle;
+import android.content.Intent;
+import android.app.Activity;
+import android.net.Uri;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class SubmissionActivity extends AppCompatActivity {
 
+    public static final int GET_FROM_GALLERY = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submission);
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         this.getSupportActionBar().setHomeAsUpIndicator(R.drawable.back_arrow);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    public void uploadPhoto(View v) {
+        startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        //Detects request codes
+        if(requestCode==GET_FROM_GALLERY && resultCode == Activity.RESULT_OK) {
+            Uri selectedImage = data.getData();
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
