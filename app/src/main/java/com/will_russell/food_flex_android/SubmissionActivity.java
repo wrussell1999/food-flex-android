c40850f6f0e7634977b7d28073a136828d488ca1
wrussell1999 <jetpowerss@gmail.com>
2019-01-23 03:24:28 +0000

    Both Camera and Galley display image to ImageView

--- app/src/main/java/com/will_russell/food_flex_android/SubmissionActivity.java
+++ app/src/main/java/com/will_russell/food_flex_android/SubmissionActivity.java
@@ -2,6 +2,7 @@ package com.will_russell.food_flex_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import android.view.View;
 import android.os.Bundle;
 import android.content.Intent;
@@ -11,16 +12,21 @@ import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import java.io.IOException;
 import java.io.FileNotFoundException;
+import android.widget.ImageView;
 
 public class SubmissionActivity extends AppCompatActivity {
 
-    public static final int GET_FROM_GALLERY = 3;
+    static final int GET_FROM_GALLERY = 3;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView mImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submission);
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         this.getSupportActionBar().setHomeAsUpIndicator(R.drawable.back_arrow);
+        mImageView = (ImageView) findViewById(R.id.submission_preview);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,24 +40,35 @@ public class SubmissionActivity extends AppCompatActivity {
         startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY);
     }
 
+    public void takePhoto(View v) {
+        dispatchTakePictureIntent();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-
-        //Detects request codes
         if(requestCode==GET_FROM_GALLERY && resultCode == Activity.RESULT_OK) {
             Uri selectedImage = data.getData();
             Bitmap bitmap = null;
             try {
                 bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                mImageView.setImageBitmap(bitmap);
             } catch (FileNotFoundException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             } catch (IOException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
+        } else if (requestCode==REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
         }
     }
 
